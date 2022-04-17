ab085c4d066271a30c492dfca33bee83ca796af0
Juha Lehtinen <jtlehtinen@fastmail.com>
2022-04-17 19:24:55 +0300

    selection sort

--- sort/selection.go
+++ sort/selection.go
@@ -5,5 +5,17 @@ import (
 )
 
 func Selection[T constraints.Ordered](x []T) []T {
+	n := len(x)
+
+	for i := 0; i < n-1; i++ {
+		min := i
+		for j := i + 1; j < n; j++ {
+			if x[j] < x[min] {
+				min = j
+			}
+		}
+		x[i], x[min] = x[min], x[i]
+	}
+
 	return x
 }
